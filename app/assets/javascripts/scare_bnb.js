5c98c6487f96573365095f6e1a1370fe7495bb5e
Andrew Strachan <andrewastrachan@outlook.com>
2014-11-25 17:47:00 -0800

    Backbone time

---
+++ app/assets/javascripts/scare_bnb.js
@@ -0,0 +1,14 @@
+window.ScareBnb = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+		
+  }
+};
+
+$(document).ready(function(){
+  ScareBnb.initialize();
+});
